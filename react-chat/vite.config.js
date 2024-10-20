37e2044b8ec48bf017ce4cdb4cf178a4877ab205
MDayZZ <maxdayzzoff@gmail.com>
2024-10-20 04:57:16 +0300

    changed config

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://mDayZZ.github.io/2024-2-VK-EDU-Frontend-M-Seregin/",
+  base: "/2024-2-VK-EDU-Frontend-M-Seregin/",
   plugins: [react()],
 })
