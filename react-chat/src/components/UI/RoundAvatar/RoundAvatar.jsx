4f92b5394156086114e705fc1748cade4ec541a9
MDayZZ <maxdayzzoff@gmail.com>
2024-10-20 04:52:01 +0300

    change paths

--- react-chat/src/components/UI/RoundAvatar/RoundAvatar.jsx
+++ react-chat/src/components/UI/RoundAvatar/RoundAvatar.jsx
@@ -3,9 +3,9 @@ import classes from './RoundAvatar.module.scss';
 const RoundAvatar = ({src = null, className}) => {
     const avatarClasses = [className, classes.roundAvatar].join(' ').trim();
     return (
-        <img className={avatarClasses} src={src ? src : '/images/avatars/default_avatar.png'} onError={(e) => {
-            e.target.onerror = null; // предотвратить бесконечный цикл в случае, если дефолтная картинка тоже не загрузится
-            e.target.src = "/images/avatars/default_avatar.png"; // подставляем дефолтную картинку
+        <img className={avatarClasses} src={src ? src : 'images/avatars/default_avatar.png'} onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = "images/avatars/default_avatar.png";
         }} />
     );
 };
