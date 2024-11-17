43385bfe687fde70065d771b9f310faa52dd1d0a
MDayZZ <maxdayzzoff@gmail.com>
2024-11-17 04:17:06 +0300

    fix: file couldn't be added after removing

--- react-chat/src/hooks/useAttachFiles.js
+++ react-chat/src/hooks/useAttachFiles.js
@@ -9,10 +9,13 @@ export const useAttachFiles = () => {
         const files = e.target.files;
         const filesArray = Array.from(files);
         setAttachedFiles(prev => [...prev, ...filesArray]);
+        fileInputRef.current.value = null;
     }
 
     const onDeleteAttachedFile = (index) => {
         setAttachedFiles(prev => prev.filter((file, fileIndex) => fileIndex !== index));
+
+
     }
 
     const onFilesSent = () => {
