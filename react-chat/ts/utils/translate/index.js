d7da3f5a05b2252da5038bbc153f2b8a39f19770
MDayZZ <maxdayzzoff@gmail.com>
2024-12-29 02:58:50 +0300

    deleted unnecessary files

--- react-chat/ts/utils/translate/index.js
+++
@@ -1,2 +0,0 @@
-import * as TranslateUtils from './module';
-export default TranslateUtils;
--- react-chat/ts/utils/translate/testTranslate.js
+++
@@ -1,8 +0,0 @@
-import translate from "./module";
-const startTesting = async () => {
-    const translatedText = await translate({ from: 'ru', to: 'en', text: 'привет бротишка' });
-    console.log('бу испугался');
-    console.log(translatedText);
-};
-console.log('te');
-startTesting();
